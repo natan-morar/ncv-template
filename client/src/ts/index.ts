c04aedea1524ee46c94a2cea0942bf42032382da
Florentin Szomoru <florentin.szomoru@nc-vision.com>
2021-06-27 11:59:36 +0300

    Add rest api and socket.io sample functionality

--- client/src/ts/index.ts
+++ client/src/ts/index.ts
@@ -7,7 +7,24 @@ export class App {
     init() {
         $("h1").text($("h1").text() + " from NC Vision");
 
+        $('.btn-increment').on("click", () =>{
+            socket.emit("increment");
+        });
+
+        $('.btn-check').on("click", () =>{
+            $.getJSON("api/v1/system", (data) => {
+                $('.load').text(`${data[0]}%` );
+                $('.mem').text(`${(data[1]/data[2]*100).toFixed(2)}%` );
+            });
+        });
+        
+
+
         socket.on("connected", this.onSocketConnection);
+
+        socket.on("counter", (d) => {
+            $(".counter").text(d);
+        });
     }
 
     private onSocketConnection = (message) => {
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import * as path from "path";
 import * as http from "http";
 import * as socket from "socket.io";
+import os from "os";
 
 const app = express();
 const PORT = 3333;
@@ -11,10 +12,14 @@ const publicPath = path.join(__dirname, "../client");
 export class App {
     private clientsMap: Map<string, socket.Socket> = new Map();
 
+    private counter:number = 0;
+    private cpuLoad: number;
+    
     constructor() {
         this.initSocket();
         this.registerRoutes();
         this.runServer();
+        this.monitorCpu();
     }
 
     private runServer() {
@@ -22,6 +27,55 @@ export class App {
             console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
         });
     }
+
+    private monitorCpu() {
+                //Grab first CPU Measure
+        var startMeasure = this.cpuAverage();
+
+        //Set delay for second Measure
+        setTimeout(()  =>{ 
+
+            //Grab second Measure
+            var endMeasure = this.cpuAverage(); 
+
+            //Calculate the difference in idle and total time between the measures
+            var idleDifference = endMeasure.idle - startMeasure.idle;
+            var totalDifference = endMeasure.total - startMeasure.total;
+
+            //Calculate the average percentage CPU usage
+            this.cpuLoad = 100 - ~~(100 * idleDifference / totalDifference);
+
+            this.monitorCpu();
+
+        }, 1000);
+    }
+
+    private cpuAverage() {
+
+        //Initialise sum of idle and time of cores and fetch CPU info
+        var totalIdle = 0, totalTick = 0;
+        var cpus = os.cpus();
+      
+        //Loop through CPU cores
+        for(var i = 0, len = cpus.length; i < len; i++) {
+      
+          //Select CPU core
+          var cpu:any = cpus[i];
+
+          let type:string;
+
+          //Total up the time in the cores tick
+          for(type in cpu.times) {
+            totalTick += cpu.times[type];
+         }     
+      
+          //Total up the idle time of the core
+          totalIdle += cpu.times.idle;
+        }
+      
+        //Return the average Idle and Tick times
+        return {idle: totalIdle / cpus.length,  total: totalTick / cpus.length};
+      }
 
     private initSocket() {
         const io = socket.listen(server);
@@ -36,6 +90,12 @@ export class App {
                 this.clientsMap.delete(socket.id);
                 console.log(`Socket ${socket.id} disconnected`);
             });
+
+            socket.on("increment", () => {
+                this.counter++;
+                io.emit("counter",this.counter);
+            });
+
         });
     }
 
@@ -43,6 +103,12 @@ export class App {
         app.use("/", express.static(publicPath));
         // app.get("/2", (req, res) => res.sendFile(path.join(publicPath + "/index.html")));
         app.get("/1", (req, res) => res.send("Express + TypeScript Server 1"));
+
+        app.get("/api/v1/system", (req, res) => {
+            
+            res.json([this.cpuLoad,os.freemem(),os.totalmem()]);
+        });
+
     }
 }
 
